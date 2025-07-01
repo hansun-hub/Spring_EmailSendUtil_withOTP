0a3b042e36e71e1c4078383c7b400b0a4f78e224
hansun-hub <hansun33@naver.com>
2025-07-01 20:54:24 +0900

    email_OTP_Util/ Swagger & Router

---
+++ src/main/java/com/example/demo/domain/auth/controller/AccountController.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain.auth.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name="Account API", description = "계정 관련 API")
+@RestController
+@RequestMapping("/api/v1/account")
+@RequiredArgsConstructor
+public class AccountController {
+
+    //Service
+    @Operation(
+            summary = "Email에 OTP 전송",
+            description = "Email에 대해서 OTP를 전송합니다"
+    )
+    @GetMapping("/make-user/{email}")
+    public String sendOTP(){
+    return "test";
+    }
+}
